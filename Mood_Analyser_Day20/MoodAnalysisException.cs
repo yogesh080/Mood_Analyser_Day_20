8ba21406ce4cef934bdb8c212186f8427a995274
Yogesh <yogeshbietjhs@gmail.com>
2022-07-23 10:06:54 +0530

    Handle exception is addedd

---
+++ Mood_Analyser_Day20/MoodAnalysisException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser_Day20
+{
+    public class MoodAnalysisException : Exception
+    {
+        public MoodAnalysisException(string message) : base(message)
+        {
+
+        }
+    }
+}
