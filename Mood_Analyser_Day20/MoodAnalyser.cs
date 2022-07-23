6b1d0207150c1ad8d3175e123b28a0bb590af6f3
Yogesh <yogeshbietjhs@gmail.com>
2022-07-23 09:59:50 +0530

    Incial commit

---
+++ Mood_Analyser_Day20/MoodAnalyser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser_Day20
+{
+    public class MoodAnalyser
+    {
+
+        public string AnalyseMood(string message)
+        {
+            if (message.Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+
+    }
+}
---
+++ Mood_Analyser_Day20/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MoodAnalyser_Day20
+{
+    
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to mood analyser");
+
+
+        }
+    }
+}
